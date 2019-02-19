dd5b62fcd3ec6cb66de6e2f62244157b33aeecac
koukyo1994 <arabiannight1994@yahoo.co.jp>
2019-02-19 23:05:29 +0900

    特徴抽出のコードは書いた

---
+++ script/lstm-attention/core/feature_extraction.py
@@ -0,0 +1,86 @@
+import gc
+
+import numpy as np
+import pandas as pd
+import pyarrow.parquet as pq
+
+from tsfresh.feature_extraction import extract_features
+
+from tqdm import tqdm
+
+from core.utils import min_max_transf
+
+
+def fresh_features(path="../input/train.parquet",
+                   offset=0,
+                   ncols=1452,
+                   n_dims=160,
+                   fc_parameters={}):
+    nrows = 800000 * ncols
+    pq_mat = np.zeros((nrows, 3))
+    bucket_size = int(800000 / n_dims)
+
+    idx = np.repeat(range(n_dims), bucket_size)
+    dummy = np.tile(idx, ncols).reshape(-1, 1)
+    group = np.repeat(range(ncols), 800000).reshape(-1, 1)
+    pq_mat = np.concatenate([pq_mat, dummy, group], axis=1)
+    del dummy, group
+    gc.collect()
+
+    temp = pq.read_pandas(
+        path, columns=[str(offset),
+                       str(offset + 1),
+                       str(offset + 2)]).to_pandas().values
+    pq_mat[0:800000, 0:3] = temp
+
+    for i in tqdm(range(1, ncols)):
+        temp = pq.read_pandas(
+            path,
+            columns=[
+                str(offset + i),
+                str(offset + i + 1),
+                str(offset + i + 2)
+            ]).to_pandas().values
+        pq_mat[i * 800000:(i + 1) * 800000, 0:3] = temp
+    del temp
+    gc.collect()
+    parq = pd.DataFrame(
+        pq_mat, columns=["phase0", "phase1", "phase2", "dummy", "group"])
+    n_feats = 0
+    for v in fc_parameters.values():
+        if isinstance(v, list):
+            n_feats += len(n_feats)
+        else:
+            n_feats += 1
+    feat_mat = np.zeros((ncols, n_dims, 3 * n_feats))
+    for i in range(3):
+        feats = extract_features(
+            parq,
+            default_fc_parameters=fc_parameters,
+            column_id="dummy",
+            column_kind="group",
+            column_value=f"phase{i}",
+            n_jobs=2)
+        for j in range(ncols):
+            feats_per_cols = feats.filter(
+                regex=f"^{float(j)}__").values.reshape((-1, n_dims, n_feats))
+            feat_mat[j, :, i * n_feats:(i + 1) * n_feats] = feats_per_cols
+    return feat_mat
+
+
+class Transformer:
+    def __init__(self,
+                 logger,
+                 sample_size=800000,
+                 n_dim=160,
+                 min_max=(-1, 1),
+                 min_num=-128,
+                 max_num=127):
+        self.n_dim = n_dim
+        self.min_max = min_max
+        self.min_num = min_num
+        self.max_num = max_num
+        self.sample_size = sample_size
+        self.logger = logger
+
+        self.bucket_size = int(sample_size / n_dim)
--- script/lstm-attention/core/utils.py
+++ script/lstm-attention/core/utils.py
@@ -1,3 +1,10 @@
+import sys
+import time
+import logging
+import datetime as dt
+
+from pathlib import Path
+
 from sklearn.metrics import matthews_corrcoef
 
 
@@ -12,3 +19,46 @@ def threshold_search(y_true, y_proba):
             best_score = score
     search_result = {"threshold": best_threshold, "mcc": best_score}
     return search_result
+
+
+def min_max_transf(ts, min_data, max_data, range_needed=(-1, 1)):
+    if min_data < 0:
+        ts_std = (ts + abs(min_data)) / (max_data + abs(min_data))
+    else:
+        ts_std = (ts - min_data) / (max_data + abs(min_data))
+    if range_needed[0] < 0:
+        return ts_std * (
+            range_needed[1] + abs(range_needed[0])) + range_needed[0]
+    else:
+        return ts_std * (range_needed[1] - range_needed[0]) + range_needed[0]
+
+
+def timer(name, logger=None):
+    t0 = time.time()
+    yield
+    msg = f"[{name}] done in {time.time()-t0:.0f} s"
+    if logger:
+        logger.info(msg)
+    else:
+        print(msg)
+
+
+def get_logger(name="Main", tag="exp", log_dir="log/"):
+    log_path = Path(log_dir)
+    path = log_path / tag
+    path.mkdir(exist_ok=True)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    fh = logging.FileHandler(
+        path / dt.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + ".log")
+    sh = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter(
+        "%(asctime)s %(name)s %(levelname)s %(message)s")
+
+    fh.setFormatter(formatter)
+    sh.setFormatter(formatter)
+    logger.addHandler(fh)
+    logger.addHandler(sh)
+    return logger
