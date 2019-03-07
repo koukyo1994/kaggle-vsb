45febb8cf3a3291d6b5c927c85b0de7608e74e8b
koukyo1994 <arabiannight1994@yahoo.co.jp>
2019-03-07 16:26:30 +0900

    av-lstm-attention完成

---
+++ script/av-lstm-attention/train.py
@@ -0,0 +1,77 @@
+import sys
+import pickle
+
+from pathlib import Path
+
+from argparse import ArgumentParser
+
+if __name__ == "__main__":
+    sys.path.append("../..")
+    sys.path.append("..")
+    sys.path.append("./")
+    from model import LSTMAttentionNet
+    from script.common.utils import get_logger
+    from trainer import NNTrainer
+
+    parser = ArgumentParser()
+    parser.add_argument("--hidden_size", default=128, type=int)
+    parser.add_argument("--linear_size", default=100, type=int)
+    parser.add_argument("--n_attention", default=50, type=int)
+
+    parser.add_argument("--train_batch", default=128, type=int)
+
+    parser.add_argument("--n_splits", default=5, type=int)
+    parser.add_argument("--seed", default=42, type=int)
+
+    parser.add_argument("--device", default="cpu")
+
+    parser.add_argument("--n_epochs", default=50, type=int)
+
+    parser.add_argument("--train_set")
+    parser.add_argument("--validation_set")
+
+    args = parser.parse_args()
+
+    logger = get_logger("av-lstm-attention", "av-lstm-attention")
+    logger.info(
+        f"hidden_size: {args.hidden_size}, linear_size: {args.linear_size}")
+    logger.info(f"n_attention: {args.n_attention}")
+    logger.info(f"train_batch: {args.train_batch}")
+    logger.info(f"n_splits: {args.n_splits}, seed: {args.seed}")
+    logger.info(f"n_epochs: {args.n_epochs}")
+    logger.info(f"train_set: {args.train_set}")
+    logger.info(f"validation_set: {args.validation_set}")
+
+    with open(args.train_set, "rb") as f:
+        train_set = pickle.load(f)
+
+    with open(args.validation_set, "rb") as f:
+        validation_set = pickle.load(f)
+
+    trainer = NNTrainer(
+        LSTMAttentionNet,
+        logger,
+        validation_set,
+        n_splits=5,
+        seed=42,
+        device="cpu",
+        train_batch=128,
+        kwargs={
+            "hidden_size": args.hidden_size,
+            "linear_size": args.linear_size,
+            "input_shape": train_set[0].shape,
+            "n_attention": args.n_attention
+        })
+
+    trainer.fit(train_set[0], train_set[1], args.n_epochs)
+    trainer_path = Path(f"trainer/{trainer.tag}")
+    trainer_path.mkdir(parents=True, exist_ok=True)
+
+    trainer.fold = None
+    trainer.valid_set = None
+    trainer.local_loader = None
+    trainer.model = None
+    trainer.loss_fn = None
+    trainer.logger = None
+    with open(trainer_path / "trainer.pkl", "wb") as f:
+        pickle.dump(trainer, f)
--- script/av-lstm-attention/trainer.py
+++ script/av-lstm-attention/trainer.py
@@ -9,7 +9,7 @@ import numpy as np
 
 from pathlib import Path
 
-from sklearn.model_selection import StratifiedKFold, train_test_split
+from sklearn.model_selection import StratifiedKFold
 
 from script.common.utils import timer
 from script.common.train_helpers import (sigmoid, threshold_search, seed_torch)
@@ -45,17 +45,21 @@ class Trainer:
             y_train = self.y[trn_index]
             y_val = self.y[val_index]
 
-            valid_preds = self._fit(
+            valid_preds, local_preds = self._fit(
                 X_train, y_train, n_epochs, eval_set=(X_val, y_val))
             self.train_preds[val_index] = valid_preds
         search_result = threshold_search(self.y, self.train_preds)
+        search_result_loc = threshold_search(self.valid_set[1], local_preds)
 
         self.logger.info(f"MCC: {search_result['mcc']}")
         self.logger.info(f"threshold: {search_result['threshold']}")
+        self.logger.info(f"Local MCC: {search_result_loc['mcc']}")
+        self.logger.info(f"Local threshold: {search_result_loc['threshold']}")
+
         self.best_score = search_result['mcc']
         self.best_threshold = search_result['threshold']
-
-        self._score(self.valid_set, self.valid_y)
+        self.av_score = search_result_loc['mcc']
+        self.av_threshold = search_result_loc['threshold']
 
 
 class NNTrainer(Trainer):
@@ -67,16 +71,24 @@ class NNTrainer(Trainer):
                  seed=42,
                  device="cpu",
                  train_batch=128,
-                 val_batch=512,
                  kwargs={}):
         super().__init__(logger, av_valid, n_splits=n_splits, seed=seed)
         self.model = model
         self.device = device
         self.kwargs = kwargs
         self.train_batch = train_batch
-        self.val_batch = val_batch
+        self.val_batch = 128
 
         self.loss_fn = nn.BCEWithLogitsLoss(reduction="mean").to(self.device)
+
+        local_test = torch.tensor(
+            self.valid_set[0], dtype=torch.float32).to(self.device)
+        local_test_y = torch.tensor(
+            self.valid_set[1][:, np.newaxis],
+            dtype=torch.float32).to(self.device)
+        local_set = torch.utils.data.TensorDataset(local_test, local_test_y)
+        self.local_loader = torch.utils.data.DataLoader(
+            local_set, batch_size=128, shuffle=False)
 
         path = Path(f"bin/{self.tag}")
         path.mkdir(exist_ok=True, parents=True)
@@ -84,4 +96,82 @@ class NNTrainer(Trainer):
 
     def _fit(self, X_train, y_train, n_epochs=50, eval_set=()):
         seed_torch()
-        x_train = torch.tensor(X_train, dtype=torch.float32)
+        x_train = torch.tensor(X_train, dtype=torch.float32).to(self.device)
+        y = torch.tensor(
+            y_train[:, np.newaxis], dtype=torch.float32).to(self.device)
+
+        train = torch.utils.data.TensorDataset(x_train, y)
+        train_loader = torch.utils.data.DataLoader(
+            train, batch_size=self.train_batch, shuffle=True)
+
+        x_val = torch.tensor(eval_set[0], dtype=torch.float32).to(self.device)
+        y_val = torch.tensor(
+            eval_set[1][:, np.newaxis], dtype=torch.float32).to(self.device)
+        valid = torch.utils.data.TensorDataset(x_val, y_val)
+        valid_loader = torch.utils.data.DataLoader(
+            valid, batch_size=self.val_batch, shuffle=False)
+
+        model = self.model(**self.kwargs)
+        model.to(self.device)
+        optimizer = optim.Adam(model.parameters())
+        best_score = -np.inf
+        best_loc_score = -np.inf
+
+        for epoch in range(n_epochs):
+            with timer(f"Epoch {epoch+1}/{n_epochs}", self.logger):
+                model.train()
+                avg_loss = 0.
+                for (x_batch, y_batch) in train_loader:
+                    y_pred = model(x_batch)
+                    loss = self.loss_fn(y_pred, y_batch)
+                    optimizer.zero_grad()
+
+                    loss.backward()
+                    optimizer.step()
+                    avg_loss += loss.item() / len(train_loader)
+
+                valid_preds, avg_val_loss = self._val(valid_loader, model)
+                search_result = threshold_search(eval_set[1], valid_preds)
+                val_mcc, val_threshold = search_result["mcc"], search_result[
+                    "threshold"]
+
+                local_preds, avg_loc_loss = self._val(self.local_loader, model)
+                search_result_loc = threshold_search(self.valid_set[1],
+                                                     local_preds)
+                loc_mcc, loc_threshold = search_result_loc[
+                    "mcc"], search_result_loc["threshold"]
+            self.logger.info(
+                f"loss: {avg_loss:.4f} val_loss: {avg_val_loss:.4f}")
+            self.logger.info(f"val_mcc: {val_mcc:.4f} best_t: {val_threshold}")
+            self.logger.info(f"loc_mcc: {loc_mcc:.4f} loc_t: {loc_threshold}")
+            if val_mcc > best_score and loc_mcc > best_loc_score:
+                torch.save(model.state_dict(),
+                           self.path / f"best{self.fold}.pt")
+                self.logger.info(f"Save model on epoch {epoch+1}")
+                best_score = val_mcc
+                best_loc_score = loc_mcc
+            elif val_mcc > best_score:
+                best_score = val_mcc
+            elif loc_mcc > best_loc_score:
+                best_loc_score = loc_mcc
+        model.load_state_dict(torch.load(self.path / f"best{self.fold}.pt"))
+        valid_preds, avg_val_loss = self._val(valid_loader, model)
+        local_preds, avg_loc_loss = self._val(self.local_loader, model)
+        self.logger.info(f"Validation loss: {avg_val_loss:.4f}")
+        self.logger.info(f"local loss: {avg_loc_loss:.4f}\n")
+        return valid_preds, local_preds
+
+    def _val(self, loader, model):
+        model.eval()
+        valid_preds = np.zeros(loader.dataset.tensors[0].size(0))
+        avg_val_loss = 0.
+
+        for i, (x_batch, y_batch) in enumerate(loader):
+            with torch.no_grad():
+                y_pred = model(x_batch).detach()
+                avg_val_loss += self.loss_fn(y_pred,
+                                             y_batch).item() / len(loader)
+                valid_preds[i * self.val_batch:(i + 1) *
+                            self.val_batch] = sigmoid(
+                                y_pred.cpu().numpy())[:, 0]
+        return valid_preds, avg_val_loss
